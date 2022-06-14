c556b756bd2b04ab27df770b30ef7fd98458681a
solawc <1225093964@qq.com>
2022-06-14 11:26:23 +0800

    add py file and rename

---
+++ platformIO/scrript/rename.py
@@ -0,0 +1,7 @@
+Import("env")
+
+my_flags = env.ParseFlags(env['BUILD_FLAGS'])
+defines = {k: v for (k, v) in my_flags.get("CPPDEFINES")}
+# print(defines)
+
+env.Replace(PROGNAME="%s" % defines.get("NAME"))
