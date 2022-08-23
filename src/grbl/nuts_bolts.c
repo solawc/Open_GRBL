b7dfa8de7b7f695838fdbcd3c528b734267c5fd8
solawc <1225093964@qq.com>
2022-08-23 19:18:50 +0800

    fix delay_us

--- src/grbl/nuts_bolts.c
+++ src/grbl/nuts_bolts.c
@@ -141,21 +141,22 @@ void delay_ms(uint16_t ms)
 // efficiently with larger delays, as the counter adds parasitic time in each iteration.
 void delay_us(uint32_t us)
 {
-  while (us) {
-    if (us < 10) {
-      _delay_us(1);
-      us--;
-    } else if (us < 100) {
-      _delay_us(10);
-      us -= 10;
-    } else if (us < 1000) {
-      _delay_us(100);
-      us -= 100;
-    } else {
-      _delay_ms(1);
-      us -= 1000;
-    }
-  }
+  // while (us) {
+  //   if (us < 10) {
+  //     _delay_us(1);
+  //     us--;
+  //   } else if (us < 100) {
+  //     _delay_us(10);
+  //     us -= 10;
+  //   } else if (us < 1000) {
+  //     _delay_us(100);
+  //     us -= 100;
+  //   } else {
+  //     _delay_ms(1);
+  //     us -= 1000;
+  //   }
+  // }
+  _delay_us(us);
 }
 
 
