d61f0005a99b930fa30ae65e5ed81777e2bf6578
solawc <1225093964@qq.com>
2022-09-26 18:56:56 +0800

    add sdio

--- src/stm32-platformio-freertos/freertos_config_cm4f/FreeRTOSConfig.h
+++ src/stm32-platformio-freertos/freertos_config_cm4f/FreeRTOSConfig.h
@@ -71,6 +71,7 @@
 #define configUSE_MUTEXES                        1
 #define configQUEUE_REGISTRY_SIZE                8
 #define configUSE_PORT_OPTIMISED_TASK_SELECTION  1
+#define configIDLE_SHOULD_YIELD                  0
 /* USER CODE BEGIN MESSAGE_BUFFER_LENGTH_TYPE */
 /* Defaults to size_t for backward compatibility, but can be changed
    if lengths will always be less than the number of bytes in a size_t. */
